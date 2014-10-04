81c536466a1f3cd94c9b0d749b393e5dd594f809
tdshap <Sue@TessShapiro.local>
2014-10-04 12:47:48 -0400

    basic contacts manager

---
+++ seeds.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require_relative './db/connection'
+require_relative './lib/category'
+require_relative './lib/contact'
+
+Category.delete_all
+Contact.delete_all
+
+friends = Category.create(name: "friends")
+family = Category.create(name: "family")
+work = Category.create(name: "work")
+
+Contact.create(name: "Sean", category_id: friends.id)
+Contact.create(name: "Jeff", category_id: family.id)
+Contact.create(name: "Neel", category_id: work.id)
